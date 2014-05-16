05076dbd888563c7ea1d6e272d423fbb8c9054c7
g548462 <g548462@gmail.com>
2014-05-16 01:52:48 -0400

    Add MongoDB query to score board

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
+var db_name = 'SounderBarDB';
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/SounderBarDB');
+var collection = db.get('record_collection');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+  collection.find({},{limit:5,sort:{"Score":-1}},function(err,data){
+    res.render('index',{title:'Shout2Play','score_list':data});
+  });
 };
