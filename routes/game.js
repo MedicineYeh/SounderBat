d5736385ace30a278480825e1529846860b7d883
g548462 <g548462@gmail.com>
2014-05-29 05:56:53 -0400

    add some information

--- routes/game.js
+++ routes/game.js
@@ -2,15 +2,16 @@ exports.game = function(req,res){
 	content = '' +
 		'<div id="game" style="overflow: hidden;"><\/div>' + 
     	'<div>' +
-    	  '<input type="button" onclick="gameStart();" value="GO" />' +
+    	  '<center><input type="button" class="btn btn-danger" onclick="gameStart();" value="Play" /></center>' +
     		'<p>' +
         		'<ul>' + 
             		'<li type="disc">當你喊出<b class="bg-info"><font size="4">POP</font></b>時,角色會往<b class="bg-info"><font size="4">上方</font></b>移動</li>' +
             		'<li type="disc">當你喊出<b class="bg-info"><font size="4">PUSH</font></b>時,角色會往<b class="bg-info"><font size="4">下方</font></b>移動</li>' +
             		'<li type="disc">當你喊出<b class="bg-info"><font size="4">SHOOT、SHOO~</font></b>時,角色會<b class="bg-info"><font size="4">發射棒球</font></b></li>' +
             		'<li type="disc">當你喊出<b class="bg-info"><font size="4">BOOM</font></b>時,角色會<b class="bg-info"><font size="4">使用大招</font></b>消滅所有物體</li>' +
-        		'</ul>' + 
-            '</p>' +
+        		'</ul>' +
+            '<h3 class="bg-success">本遊戲因技術上問題，目前只限Chrome瀏覽器使用</h3>' +
+        '</p>' +
         '</div>' +
         '<script src="/js/game_control.js">' + '<\/script>' + 
         '<script src="/js/game_play.js">' + '<\/script>' + 
